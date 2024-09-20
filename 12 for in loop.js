ebfa0353965510105df0976754f35eff0a796dc3
Pritam Mazumder <109612812+pritam-mazumder@users.noreply.github.com>
2024-09-20 17:56:55 +0530

    20-09-2024

---
+++ 12 for in loop.js
@@ -0,0 +1,19 @@
+agents = {
+  sova: 'initiator',
+  jett: 'duelist',
+  omen: 'controller',
+  sage: 'sentinel',
+};
+
+for (a in agents) {
+  console.log(agents[a]);
+}
+
+const numbers = [45, 4, 10, 16, 25];
+
+let num = 0;
+for (let x in numbers) {
+  num += numbers[x];
+}
+
+console.log(num);
